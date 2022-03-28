71144d1aac3fce6fdd658f10f5faa711695a7974
Rudhreswar <rudra3d4@gmail.com>
2022-03-28 21:57:50 +0530

    Initial commit

---
+++ student-Marks-Spring-data-JPA/src/main/java/com/studentMarks/StudentMarksSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.studentMarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMarksSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMarksSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ student-Marks-Spring-data-JPA/src/test/java/com/studentMarks/StudentMarksSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentMarks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMarksSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
